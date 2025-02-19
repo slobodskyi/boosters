893d6d380fbbd7734cbc29004697ed48f305430f
Oleksandr Slobodskyi <9967038+slobodskyi@users.noreply.github.com>
2025-02-19 13:52:49 +0200

    Update router.js

--- router.js
+++ router.js
@@ -1,4 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
+  
+  const confettiScript = document.createElement('script');
+  confettiScript.src = 'https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js';
+  confettiScript.defer = true;
+  document.body.appendChild(confettiScript);
+  
   // Dynamically load quiz.js
   const quizScript = document.createElement('script');
   quizScript.src = 'https://scripts.slobodskyi.com/quiz.js';
